55ef104996577410d51d7c0c408214e8db6acde5
shrechochek <stephan.martynov41@gmail.com>
2026-01-18 14:25:37 +0300

    add ability to set custom days of week and classes in library

--- schedule_parser/schedule_parser.py
+++ schedule_parser/schedule_parser.py
@@ -2,6 +2,9 @@ import requests
 import csv
 import io
 
+days_of_week = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота"]
+classes = ["5С", "6С", "7С", "8С", "9С", "10С", "10Т", "11С"]
+
 def init_table(url):
     response = requests.get(url)
     response.raise_for_status()
@@ -18,8 +21,7 @@ def init_table(url):
     return table
 
 
-def get_days_of_week_indexes(table):
-    days_of_week = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота"]
+def get_days_of_week_indexes(table, days_of_week):
     days_of_week_indexes = [0,0,0,0,0,0]
 
     for i in range(len(table)):
@@ -30,8 +32,7 @@ def get_days_of_week_indexes(table):
 
     return days_of_week_indexes
 
-def get_classes_indexes(table):
-    classes = ["5С", "6С", "7С", "8С", "9С", "10С", "10Т", "11С"]
+def get_classes_indexes(table, classes):
     classes_indexes = [0,0,0,0,0,0,0,0]
 
     for i in range(len(table[0])):
@@ -42,10 +43,10 @@ def get_classes_indexes(table):
 
     return classes_indexes
 
-def init_structured_table(url):
+def init_structured_table(url, days_of_week=days_of_week, classes=classes): # можно ставить кастомные дни недели и классы
     table = init_table(url=url)
-    days_of_week_indexes = get_days_of_week_indexes(table)
-    classes_indexes = get_classes_indexes(table)
+    days_of_week_indexes = get_days_of_week_indexes(table, days_of_week)
+    classes_indexes = get_classes_indexes(table, classes)
     strucutred_table = []
 
     for i in range(len(days_of_week_indexes)-1):
