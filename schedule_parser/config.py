f6db7f37f5866e980af04dbb312c5bc7fa3065ef
shrechochek <stephan.martynov41@gmail.com>
2026-01-20 21:11:46 +0300

    updaet config.py
    add ability to create variables in env and use them

--- schedule_parser/config.py
+++ schedule_parser/config.py
@@ -1,10 +1,17 @@
-url = (
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+#table url setting
+URL = getenv("URL", 
     "https://docs.google.com/spreadsheets/d/" # гугл таблицы
     "1W9qMX1QzlZvBkNS0lwA7ZKyMGUlR9dBZnAE9JwqHRHg/export" # id таблицы
-    "?format=csv&gid=917584427" # id страницы
+    "?format=csv&gid=917584427" # id страницы"
 )
 
-classes_start_marker = "5С" #маркер первого класса в таблице
-classes_stop_marker = "Питание в столовой" #маркер после последнего класса в таблице
-days_of_week_start_marker = "Понедельник" #маркер первого дня
-days_of_week_stop_marker = "Суббота" #маркер последнего дня
+#markers setting
+CLASSES_START_MARKER = int(getenv("CLASSES_START_MARKER", "5С"))
+CLASSES_STOP_MARKER = int(getenv("CLASSES_STOP_MARKER", "Питание в столовой"))
+DAYS_OF_WEEK_START_MARKER = int(getenv("DAYS_OF_WEEK_START_MARKER", "Понедельник"))
+DAYS_OF_WEEK_STOP_MARKER = int(getenv("DAYS_OF_WEEK_STOP_MARKER", "Суббота"))
