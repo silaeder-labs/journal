fb2dda6c2184c8ba1caf689b823c96d1e2133b25
shrechochek <stephan.martynov41@gmail.com>
2026-01-23 23:51:26 +0300

    move all important variables to config

--- mesh_parser/mesh-parser.js
+++ mesh_parser/mesh-parser.js
@@ -6,11 +6,11 @@ require('dotenv').config({ path: path.join(__dirname, '.env') });
 const authorization_token = process.env.BEARER_TOKEN;
 const subject_names = process.env.SUBJECT_NAMES.split(',');
 const subject_ids = process.env.SUBJECT_IDS.split(',');
-const URL_BASE = "https://school.mos.ru/api/ej/rating/v1/rank/class?personId=5e6f08de-bea8-40ef-97f1-9da1e806236f";
+const person_id = process.env.PERSON_ID;
+const profiel_id = process.env.PROFILE_ID
+const URL_BASE = "https://school.mos.ru/api/ej/rating/v1/rank/class";
 
 const jsonFilePath = path.join(__dirname, 'data/data.json');
-// const subject_ids = [33623636];
-// const subject_names = ['biology'];
 
 function addDay(dateStr) {
     const [year, month, day] = dateStr.split('-').map(Number);
@@ -31,7 +31,7 @@ async function getRank(url) {
             method: "GET",
             headers: {
                 "Authorization": `Bearer ${authorization_token}`,
-                "profile-id": "31835076",
+                "profile-id": profiel_id,
                 "profile-type": "student",
                 "x-mes-subsystem": "familyweb",
                 "accept": "application/json"
@@ -53,7 +53,7 @@ async function get_days_marks(curr_day, subject_id, n) {
     const tasks = [];
     for (let j = 0; j <= n; j++) {
         const day = curr_day;
-        const curr_url = `${URL_BASE}&subjectId=${subject_id}&date=${day}`;
+        const curr_url = `${URL_BASE}?personId=${person_id}&subjectId=${subject_id}&date=${day}`;
         const task = getRank(curr_url).then(marks => {
             overall_marks[day] = marks;
         });
@@ -80,7 +80,6 @@ async function main() {
         final_marks[subject_names[v]] = sortedMarks;
     }
 
-    // Сохраняем только ПОСЛЕ того, как все циклы завершены
     const jsonString = JSON.stringify(final_marks, null, 2);
     fs.writeFileSync(jsonFilePath, jsonString, 'utf8');
     console.log("completed");
